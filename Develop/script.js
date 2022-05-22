5a6713162cf27e06f7b98475d81a332d7650f4a0
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2022-05-21 22:02:16 -0600

    left notes as to-do items

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,9 @@
 var saveBtn = document.getElementById('9','10','11','12','13','14','15','16','17',)
-
-
-
+//↑ is the above string correct? or do i have to create a VAR for each ID?
 
 saveBtn.addEventListener('click', function(event){
     event.preventDefault();
 })
+
+//am i going to set the attribute to each (textarea) to line up with the class?
+//create a var that uses class types (past, present, future)
