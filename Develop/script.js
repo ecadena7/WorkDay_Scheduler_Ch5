f498e6c7aaf1a1de4f204e2da7b3a5c16ea53b5e
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2022-05-21 22:13:36 -0600

    more follow-up questions

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 var saveBtn = document.getElementById('9','10','11','12','13','14','15','16','17',)
 //↑ is the above string correct? or do i have to create a VAR for each ID?
+//↑↑ also, am i choosing the correct lable for my VAR?
+
+$(document).ready(function(){
+    var today = moment().format('MMMM Do YY, H:mm:ss a')
+    $("#currentDay")
+})
 
 saveBtn.addEventListener('click', function(event){
     event.preventDefault();
