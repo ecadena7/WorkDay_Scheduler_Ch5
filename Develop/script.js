61c475bdce0a2295816a5f161af59e18d90d263f
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2022-05-24 18:45:37 -0600

    updated files

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,13 @@
-var saveBtn = document.getElementById('9','10','11','12','13','14','15','16','17',)
-//↑ is the above string correct? or do i have to create a VAR for each ID?
-//↑↑ also, am i choosing the correct lable for my VAR?
-
 $(document).ready(function(){
+
+    //my id's are going to be used in the (moment - code).
     localStorage.setItem();
-})
 
-saveBtn.addEventListener('click', function(event){
-    event.preventDefault();
+    $('.saveBtn').on('click', function(){
+        console.log($(this))
+        localStorage.setItem();
+    })
+    
+    //am i going to set the attribute to each (textarea) to line up with the class?
+    //create a var that uses class types (past, present, future)
 })
-
-//am i going to set the attribute to each (textarea) to line up with the class?
-//create a var that uses class types (past, present, future)
